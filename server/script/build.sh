1ba31aa0d672482f9219b4d93c21e980bff67b6f
huzch <huzch123@gmail.com>
2025-10-03 13:42:54 +0800

    调整构建结构

--- server/script/build.sh
+++ server/script/build.sh
@@ -8,10 +8,10 @@ mkdir -p ../service/message/build
 mkdir -p ../service/friend/build
 mkdir -p ../service/gateway/build
 
-cp -r ../build/service/speech ../service/speech/build
-cp -r ../build/service/file ../service/file/build
-cp -r ../build/service/user ../service/user/build
-cp -r ../build/service/forward ../service/forward/build
-cp -r ../build/service/message ../service/message/build
-cp -r ../build/service/friend ../service/friend/build
-cp -r ../build/service/gateway ../service/gateway/build
+cp -r ../build/service/speech/* ../service/speech/build
+cp -r ../build/service/file/* ../service/file/build
+cp -r ../build/service/user/* ../service/user/build
+cp -r ../build/service/forward/* ../service/forward/build
+cp -r ../build/service/message/* ../service/message/build
+cp -r ../build/service/friend/* ../service/friend/build
+cp -r ../build/service/gateway/* ../service/gateway/build
