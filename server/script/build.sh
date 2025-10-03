0c0bd7019e16b6c3f88a93df1d95fbe2ea68a488
huzch <huzch123@gmail.com>
2025-10-03 13:37:23 +0800

    拷贝构建目录脚本

---
+++ server/script/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p ../service/speech/build
+mkdir -p ../service/file/build
+mkdir -p ../service/user/build
+mkdir -p ../service/forward/build
+mkdir -p ../service/message/build
+mkdir -p ../service/friend/build
+mkdir -p ../service/gateway/build
+
+cp -r ../build/service/speech ../service/speech/build
+cp -r ../build/service/file ../service/file/build
+cp -r ../build/service/user ../service/user/build
+cp -r ../build/service/forward ../service/forward/build
+cp -r ../build/service/message ../service/message/build
+cp -r ../build/service/friend ../service/friend/build
+cp -r ../build/service/gateway ../service/gateway/build
